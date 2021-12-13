0277617b147c14f464bfc9d6aa26c783cf2af5a2
peris <periklis@weatherxm.com>
2021-12-13 16:39:53 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,64 @@
+#!/usr/bin/env node
+
+const path = require('path')
+const fs = require('fs')
+const chalk = require('chalk')
+const { execFile } = require('child_process')
+
+const log = (msg, ctx = 'node') => console.log(`[${chalk.yellow(ctx)}] ${msg}`)
+
+const newLine = () => console.log('\n')
+
+const fromTemplate = () => path.join(__dirname, 'template')
+
+const copy = (from, to) =>
+  fs.copyFileSync(from, to, fs.constants.COPYFILE_EXCL)
+
+const copyDir = (from, to, makeExecutable = false) => {
+  log(chalk.blue.bold(to))
+  fs.mkdirSync(to)
+  fs.readdirSync(from, { withFileTypes: true })
+    .sort((f1, f2) => f2.isFile() - f1.isFile())
+    .forEach(f => {
+      const src = path.join(from, f.name)
+      const dest = path.join(to, f.name)
+      if (f.isDirectory()) {
+        copyDir(src, dest)
+      } else {
+        copy(src, dest)
+        if (makeExecutable) {
+          fs.chmodSync(dest, '755')
+          log(` + ${chalk.green(f.name)}`)
+        } else {
+          log(` + ${f.name}`)
+        }
+      }
+    })
+}
+
+const args = process.argv.slice(2)
+const project = (args.length) ? args[0] : 'example'
+
+log(`Creating a new project ${chalk.cyan.bold(project)}`)
+copyDir(fromTemplate(), project)
+
+newLine()
+log('Initializing git')
+execFile('git', ['init'], {
+  cwd: project
+})
+  .on('exit', code => {
+    newLine()
+    log('Importing git hooks', 'git')
+    const dest = path.join(project, '.git/hooks')
+    fs.rmSync(dest, { recursive: true, force: true })
+    copyDir(path.join(__dirname, 'hooks'), dest, true)
+  })
+  .stdout.on('data', msg => log(msg, 'git'))
+
+newLine()
+log('Installing dev dependencies')
+execFile('npm', ['install', '-D'], {
+  cwd: project
+})
+  .stdout.on('data', msg => log(msg, 'npm'))
---
+++ template/readme.md
@@ -0,0 +1,21 @@
+# Your great project
+Every great project starts with a little documentation!
+
+## Installation
+Some descriptive instructions...
+
+then some lists of important steps...
+- one step
+- at a time
+
+and perhaps a little bit of code...
+```
+bash clarify.sh
+```
+
+## Requirements
+Whatever your app needs in order to run (environment variables, configuration file etc)
+
+## Usage
+Just a couple of examples of how would someone other than you find this useful
+
